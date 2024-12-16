e7b52e913aab0fca68a485ada2eb3bd0a7555add
emo-Ab <abrahambvbenc@gmail.com>
2024-12-16 14:53:29 +0100

    Cleaned up led display class

--- raspi/led_display.py
+++ raspi/led_display.py
@@ -3,50 +3,27 @@ import time
 import threading
 from scipy import signal
 from gpiozero import LED
-try:
-    import queue as Queue
-except ImportError:
-    import Queue as Queue
 import pyaudio
 import wave
 import numpy as np
 import audioop
-from alexa_led_pattern import AlexaLedPattern
 from update_noise_detected import NoiseUpdater
 
 class LedDisplay:
     PIXELS_N = 12
 
-    def __init__(self, pattern=AlexaLedPattern):
-        self.pattern = pattern(show=self.show)
+    def __init__(self):
         self.dev = apa102.APA102(num_led=self.PIXELS_N)
         self.power = LED(5)
         self.power.on()
-        self.queue = Queue.Queue()
-        self.thread = threading.Thread(target=self._run)
-        self.thread.daemon = True
-        self.thread.start()
-        self.last_direction = None
         self.fill_level = 0
-        self.updater = NoiseUpdater('http://localhost:8080')
+        self.updater = NoiseUpdater('http://192.168.0.31:8080/')
+
 
     def show(self, data):
         for i in range(self.PIXELS_N):
             self.dev.set_pixel(i, int(data[4*i + 1]), int(data[4*i + 2]), int(data[4*i + 3]))
         self.dev.show()
-
-    def off(self):
-        self.put(self.pattern.off)
-
-    def _run(self):
-        while True:
-            func = self.queue.get()
-            self.pattern.stop = False
-            func()
-
-    def put(self, func):
-        self.pattern.stop = True
-        self.queue.put(func)
 
 
     def calibrate(self):
@@ -79,7 +56,8 @@ class LedDisplay:
         red_led = self.fill_level
         green_led = 100 - self.fill_level
 
-	# Update the noise levels to the web server
+
+	    # Update the noise levels to the web server
         self.updater.calculate_latestnoise(red_led, green_led)
 
         #print(val)
