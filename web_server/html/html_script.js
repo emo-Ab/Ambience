c60069a976543efee84873fde5773dc554f92abd
emo-Ab <abrahambvbenc@gmail.com>
2024-12-20 15:06:08 +0100

    Updated web client to be generic

--- web_server/html/html_script.js
+++ web_server/html/html_script.js
@@ -1,11 +1,11 @@
 async function fetchDeviceNames() {
-    const response = await fetch('http://192.168.0.52:8085/alldevices');
+    const response = await fetch('http://192.168.0.31:8085/alldevices');
     const text = await response.text();
     return text.split('\r\n');
 }
 
 async function fetchDeviceData(deviceName) {
-    const response = await fetch(`http://192.168.0.52:8085/?device=${deviceName}`);
+    const response = await fetch(`http://192.168.0.31:8085/?device=${deviceName}`);
     return response.text();
 }
 
@@ -16,25 +16,20 @@ async function displayDevices() {
 
     for (const deviceName of deviceNames) {
         const deviceData = await fetchDeviceData(deviceName);
-        // Parse the device data
-        const [rgb, text] = deviceData.split('\r\n');
+        const [rgb, speech] = deviceData.split('\n');
         const [r, g, b] = rgb.match(/\d+/g).map(Number);
 
         const deviceDataElement = document.createElement('div');
         deviceDataElement.className = 'device-section';
-        deviceDataElement.innerHTML = `<h2>${deviceName}</h2><p>${deviceData}</p>`;
-
-        // Add styles for the colored box
-        deviceDataElement.style.border = '1px solid #ccc';
+        deviceDataElement.innerHTML = `
+        <h2>${deviceName}</h2>
+        <p>${speech.substring(5)}</p>
+        `;
         deviceDataElement.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-        deviceDataElement.style.padding = '10px';
-        deviceDataElement.style.margin = '10px 0';
-        deviceDataElement.style.borderRadius = '5px';
-
         deviceContainer.appendChild(deviceDataElement);
     }    
 }
 
 
 // Fetch and display device data every second
-setInterval(displayDevices, 1000);
+setInterval(displayDevices, 5000);
